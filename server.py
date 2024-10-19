8f1a82b8003ec5d68208486b67b3e93ba27cc3df
Jack Drabic <drabicj@moravian.edu>
2024-10-19 13:07:46 -0400

    made server and requirements for google oauth

---
+++ server.py
@@ -0,0 +1,18 @@
+import dotenv
+import os
+import uuid
+import mysql.connector
+import pathlib
+import requests
+import google.auth.transport.requests
+import pip._vendor.cachecontrol as cachecontrol
+import time
+import redis
+from authlib.integrations.flask_client import OAuth
+from google_auth_oauthlib.flow import Flow
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+from functools import wraps
+from flask import Flask, abort, jsonify, redirect, request, session, render_template, url_for
+from flask_session import Session
+
